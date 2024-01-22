4d18dfcb95b087dee8786d0f96156e3f0af3d530
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-22 13:25:35 -0800

    Main page

---
+++ app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-700 h-screen text-3xl font-extrabold">
+      What`s up!
+    </div>
+  );
+}
