71dd0499336244f21d16f886c986da919d0c8a71
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-22 13:25:49 -0800

    removed unnecessary component

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="bg-gray-700 h-screen text-3xl font-extrabold">
-      What`s up!
-    </div>
-  );
-}
