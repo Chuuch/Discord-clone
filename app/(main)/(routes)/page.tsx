24729cb105b0ae55b18579feeb004e47aa08d625
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-23 00:47:55 -0800

    moved Home page to (main)/(routes)

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import { ModeToggle } from "@/components/mode-toggle/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="bg-zinc-800 h-screen text-3xl font-extrabold">
       <UserButton afterSignOutUrl="/"/>
+      <ModeToggle />
     </div>
   );
 }
