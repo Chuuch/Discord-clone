50547a6a2f65176297add81fade7ea181282bb16
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-23 02:08:14 -0800

    render uploaded image in modal

--- components/upload/file-upload.tsx
+++ components/upload/file-upload.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { X } from 'lucide-react';
+import Image from 'next/image';
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import '@uploadthing/react/styles.css'
+
 interface FileUploadProps {
 	endpoint: 'messageFile' | 'serverImage';
 	onChange: (url?: string) => void;
@@ -7,5 +14,29 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ endpoint, onChange, value }: FileUploadProps) => {
-	return <div>File Upload Component</div>;
+	const fileType = value?.split('.').pop();
+
+	if (value && fileType !== 'pdf') {
+		return (
+			<div className='relative h-20 w-20'>
+				<Image 
+					src={value}
+					fill
+					alt='Upload'
+					className='rounded-full'/>
+			</div>
+		)
+	}
+
+	return (
+		<UploadDropzone
+            endpoint={endpoint}
+			onClientUploadComplete={(res) => {
+				onChange(res?.[0].url);
+			}}
+			onUploadError={(error: Error) => {
+				console.log(error);
+			}}
+        />
+	);
 };
