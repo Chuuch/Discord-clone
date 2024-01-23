898a6fa8db01635af848c65be9926e583d78a1cb
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-23 03:22:38 -0800

    NavigationSidebar component

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,27 +1,33 @@
-import { currentProfile } from "@/lib/current-profile"
-import { redirect } from "next/navigation";
-import { db } from "@/lib/db";
-
+import { currentProfile } from '@/lib/current-profile';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
+import { NavigationAction } from './navigation-action';
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { NavigationItem } from './navigation-item';
+import { ModeToggle } from '@/components/mode-toggle/mode-toggle';
+import { UserButton } from '@clerk/nextjs';
 
 export const NavigationSidebar = async () => {
-    const profile = await currentProfile();
+	const profile = await currentProfile();
 
-    if (!profile) {
-        redirect('/');
-    }
+	if (!profile) {
+		redirect('/');
+	}
 
-    const servers = await db.server.findMany({
-        where: {
-            members: {
-                some: {
-                    profileId: profile.id,
-                }
-            }
-        }
-    });
+	const servers = await db.server.findMany({
+		where: {
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+	});
 
-    return (
-        <div className="
+	return (
+		<div
+			className="
             space-y-4
             flex
             flex-col
@@ -30,8 +36,39 @@ export const NavigationSidebar = async () => {
             text-primary
             w-full
             dark:bg-[#1e1f22]
-            py-3">
-            Navigation Sidebar
-        </div>
-    )
-}
+            py-3"
+		>
+			<NavigationAction />
+			<Separator
+				className="
+                    h-[2px]
+                    bg-zinc-300
+                    dark:bg-zinc-700
+                    w-10
+                    rounded-md
+                    mx-auto"
+			/>
+			<ScrollArea className="flex-1 w-full">
+				{servers.map((server) => (
+					<div key={server.id} className="mb-4">
+						<NavigationItem
+							id={server.id}
+							name={server.name}
+							imageUrl={server.imageUrl}
+						/>
+					</div>
+				))}
+			</ScrollArea>
+			<div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+				<ModeToggle />
+                <UserButton 
+                    afterSignOutUrl='/'
+                    appearance={{
+                        elements: {
+                            avatarBox: 'h-[48px] w-[48px]'
+                        }
+                    }}/>
+			</div>
+		</div>
+	);
+};
