1ad4757cac59af2f5de856a64fa9f48c9164bbf3
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-23 00:27:58 -0800

    Themes provider component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import { ThemeProvider } from '@/components/providers/theme-provider';
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -17,11 +18,22 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider appearance={{
-			baseTheme: dark
-		}}>
-			<html lang="en">
-				<body className={font.className}>{children}</body>
+		<ClerkProvider
+			appearance={{
+				baseTheme: dark,
+			}}
+		>
+			<html lang="en" suppressHydrationWarning>
+				<body className={font.className}>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="dark"
+						enableSystem={false}
+						storageKey="discord-theme"
+					>
+						{children}
+					</ThemeProvider>
+				</body>
 			</html>
 		</ClerkProvider>
 	);
