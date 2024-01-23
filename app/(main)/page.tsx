371ff38544290a22e5bff4f44ada996380582869
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-23 00:19:03 -0800

    Added Cler User menu component to main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="bg-gray-700 h-screen text-3xl font-extrabold">
-      What`s up!
+    <div className="bg-zinc-800 h-screen text-3xl font-extrabold">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
