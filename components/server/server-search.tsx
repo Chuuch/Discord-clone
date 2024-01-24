d8f7cbbeaa0fc6ac05e453f3fdd7d6443ae8a9e2
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-24 03:57:31 -0800

    server search functionality complete

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,6 +1,17 @@
 'use client';
 
 import { Search } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+
+import {
+	CommandDialog,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from '@/components/ui/command';
 
 interface ServerSearchProps {
 	data: {
@@ -17,9 +28,44 @@ interface ServerSearchProps {
 }
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
+	const [open, setOpen] = useState(false);
+	const router = useRouter();
+	const params = useParams();
+
+	useEffect(() => {
+		const down = (e: KeyboardEvent) => {
+			if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+				e.preventDefault();
+				setOpen((open) => !open);
+			}
+		};
+
+		document.addEventListener('keydown', down);
+		return () => document.removeEventListener('keydown', down);
+	}, []);
+
+	const onClick = ({
+		id,
+		type,
+	}: {
+		id: string;
+		type: 'channel' | 'member';
+	}) => {
+		setOpen(false);
+
+		if (type === 'member') {
+			return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+		}
+
+		if (type === 'channel') {
+			return router.push(`/servers/${params?.serverId}/channels/${id}`);
+		}
+	};
+
 	return (
 		<>
 			<button
+				onClick={() => setOpen(true)}
 				className="
             group 
             px-2 
@@ -67,6 +113,31 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
 					<span className="text-xs">⌘</span>K
 				</kbd>
 			</button>
+			<CommandDialog open={open} onOpenChange={setOpen}>
+				<CommandInput placeholder="Search all channels and members" />
+				<CommandList>
+					<CommandEmpty>No results found</CommandEmpty>
+					{data.map(({ label, type, data }) => {
+						if (!data?.length) return null;
+
+						return (
+							<CommandGroup key={label} heading={label}>
+								{data?.map(({ id, icon, name }) => {
+									return (
+										<CommandItem
+											key={id}
+											onSelect={() => onClick({ id, type })}
+										>
+											{icon}
+											<span>{name}</span>
+										</CommandItem>
+									);
+								})}
+							</CommandGroup>
+						);
+					})}
+				</CommandList>
+			</CommandDialog>
 		</>
 	);
 };
