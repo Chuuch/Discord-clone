aadaac3876b8ea56b1c9d28879dd875ce9993595
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-24 08:00:28 -0800

    fix: import typo on ChatHeader in ChannelId page

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,4 @@
-import ChatHeader from '@/components/chat/chat-header';
+import { ChatHeader } from '@/components/chat/chat-header';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
