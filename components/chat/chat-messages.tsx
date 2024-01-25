dd86991dd6544d0836185d8222fca28d29fafadf
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-25 06:58:10 -0800

    added loading & server crash icons to render depending on state status

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,7 +1,24 @@
 'use client';
 
-import { Member } from "@prisma/client";
+import { Fragment, useRef, ElementRef } from "react";
+import { format } from 'date-fns';
+import { Member, Message, Profile } from "@prisma/client";
+import { Loader2, ServerCrash } from "lucide-react";
+
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { useChatSocket } from "@/hooks/use-chat-socket";
+import { useChatScroll } from "@/hooks/use-chat-scroll";
+
 import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
+
+const DATE_FORMAT = 'd MMM yyyy, HH:mm';
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 interface ChatMessagesProps {
     name: string;
@@ -9,9 +26,9 @@ interface ChatMessagesProps {
     chatId: string;
     apiUrl: string;
     socketUrl: string;
-    socketQuery: Record<string, string>
+    socketQuery: Record<string, string>;
     paramKey: 'channelId' | 'conversationId';
-    parmValue: string;
+    paramValue: string;
     type: 'channel' | 'conversation'
 }
 
@@ -23,9 +40,59 @@ export const ChatMessages = ({
     socketUrl,
     socketQuery,
     paramKey,
-    parmValue,
+    paramValue,
     type,
 }: ChatMessagesProps) => {
+    const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`
+
+    const chatRef = useRef<ElementRef<'div'>>(null);
+    const bottomRef = useRef<ElementRef<'div'>>(null);
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status,
+    } = useChatQuery({
+        queryKey,
+        apiUrl,
+        paramKey,
+        paramValue,
+    });
+    useChatSocket({ queryKey, addKey, updateKey});
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0]?.items?.length ?? 0,
+    })
+
+    if (status === 'pending') {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4"/>
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading messages...
+                </p>
+            </div>
+        )
+    }
+
+    if (status === 'error') {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <ServerCrash className="h-7 w-7 text-zinc-500 my-4"/>
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Something went wrong!
+                </p>
+            </div>
+        )
+    }
+
     return (
         <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1"/>
