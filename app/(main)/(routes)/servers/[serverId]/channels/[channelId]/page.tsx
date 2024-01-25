2dda0eb05e5502dfa1160aaa47d778cf981846a2
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-25 03:36:00 -0800

    added props to ChatInput component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -52,7 +52,14 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 				<div className='flex-1'>
 					Future Messages
 				</div>
-				<ChatInput />
+				<ChatInput 
+				name={channel.name}
+				type='channel'
+				apiUrl='/api/socket/messages'
+				query={{
+					channelId: channel.id,
+					serverId: channel.serverId,
+				}}/>
 		</div>
 	);
 };
