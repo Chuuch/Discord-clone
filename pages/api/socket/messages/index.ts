ebcb1f2ba88676652f5a0a745f87540dd1105118
Chuuch <daniel.chuchulev96@gmail.com>
2024-01-25 03:35:46 -0800

    fix: POST method validation typo

--- pages/api/socket/messages/index.ts
+++ pages/api/socket/messages/index.ts
@@ -8,7 +8,7 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponseServerIo
 ) {
-	if (req.method === 'POST') {
+	if (req.method !== 'POST') {
 		return res.status(405).json({ error: 'Method not allowed' });
 	}
 
